bbe187374beeb31acdec6b85d2826df3f3d2063c
MarcoAlcazar <112454804+MarcoAlcazar@users.noreply.github.com>
2024-08-27 19:11:48 -0700

    update

--- wizard.gd
+++ wizard.gd
@@ -1,16 +1,23 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 
 @onready var animated_sprite = $animated_sprite
+var currentHealth: int 
+var maxHealth: int = 100
 
+
+func _ready() -> void:
+	currentHealth = maxHealth
+	var enemies = get_tree().get_nodes_in_group("Enemies")
+	for enemy in enemies:
+		print("Enemy:", enemy.name)
+	
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-
-	# Handle jump.
+	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
@@ -39,3 +46,17 @@ func update_animation(direction: Vector2) -> void:
 			animated_sprite.play("up")
 		elif direction.y > 0:
 			animated_sprite.play("down")
+			
+func take_damage(amount: int):
+	currentHealth -= amount
+	print_debug(currentHealth)
+	
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemies"):  # Assuming the player is in the "Player" group
+		print("Player entered Detection Area")
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Enemies"):  # Assuming the player is in the "Player" group
+		print("Player entered Detection Area")
